1f9d3c72cd06fdbc17386f70379f90befbe83dc4
refineriap <refineriap9@gmail.com>
2024-11-30 17:18:12 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-
+import Navbar from "./components/Navbar"
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Navbar() {
+    return (
+        <header className="">
+            <nav className="">
+                <img src="" alt="" className="" />
+                <span className="">this is working!!</span>
+            </nav>
+        </header>
+    )
+}
