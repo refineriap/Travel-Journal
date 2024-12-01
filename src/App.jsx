cece0e7e6a6b95d269cd25f6a35bd9b4a7429121
refineriap <refineriap9@gmail.com>
2024-12-01 10:50:26 -0500

    japan container

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ function App() {
   return (
     <>
       <Navbar />
-      <Card />
+      <main className="container">
+        <Card />
+      </main>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,17 @@ import location from "/location.svg"
 
 export default function Card() {
     return (
-        <article className="">
-            <img src="/src/assets/chris-yang-LA57RXF1mSI-unsplash.jpg" alt="" className="" />
-            <div className="">
-                <img src={location} alt="" className="" />
+        <article className="article--entry">
+            <div className="article-image-container">
+                <img src="/tokyo.jpg" alt="" className="article-main-image" />
+            </div>
+            <div className="article--info">
+                <img src={location} alt="" className="article-info-marker" />
+                <span className="article--country">Japan</span>
+                <a href="https://maps.app.goo.gl/UNk7Kx4SKgwi9QFb9" className="">View on Google Maps</a>
+                <h2 className="">Tokyo</h2>
+                <p className="">12 Jan, 2024 - 24 Jan, 2024</p>
+                <p className="">Tokyo, Japan’s busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods. The Imperial Palace sits amid large public gardens. </p>
             </div>
         </article>
     )
